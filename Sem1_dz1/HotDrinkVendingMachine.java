4603857e75016a3081fede31fdc66eead7527b3f
Irina041708 <q-w-i-n-86@rambler.ru>
2022-12-20 19:29:10 +0400

    Sem1_dz1/HotDrinkVendingMachine.java

--- Sem1_dz1/HotDrinkVendingMachine.java
+++ Sem1_dz1/HotDrinkVendingMachine.java
@@ -10,5 +10,11 @@ public class HotDrinkVendingMachine extends VendingMachine{
     public HotDrinkVendingMachine(Integer id) {
         super(id);
     }
+    public Product getProduct(String name, Integer price, Integer temperaturas) {
+        for (Product p : getProducts())
+            if (p instanceof HotDrink && p.getName().equals(name) && p.getPrice().equals(price) && ((HotDrink)p).getTemperatura() == temperaturas)
+                return p;
+        return null;
+    }
 
 }
