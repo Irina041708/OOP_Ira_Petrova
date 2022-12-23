b532bc44b9a044f8a3b24ac136f42b6ae455ad8a
Irina041708 <q-w-i-n-86@rambler.ru>
2022-12-23 18:24:57 +0400

    Sem2_dz2/controller/VendingMachineController.java

--- Sem2_dz2/controller/VendingMachineController.java
+++ Sem2_dz2/controller/VendingMachineController.java
@@ -2,14 +2,11 @@ package Sem2_dz2.controller;
 
 import Sem2_dz2.entityes.Menu;
 import Sem2_dz2.entityes.HotDrink;
-// import Sem2_dz2.serviceis.DoughServices;
 import Sem2_dz2.serviceis.HotDrinkService;
-// import Sem2_dz2.view.GetDoughByNameResponse;
 import Sem2_dz2.view.GetHotDrinkResponse;
 
 public class VendingMachineController {
 
-    // private final DoughServices doughServices;
     private final HotDrinkService hotDrinkService;
 
     public VendingMachineController( HotDrinkService hotDrinkService) {
@@ -17,10 +14,7 @@ public class VendingMachineController {
         this.hotDrinkService = hotDrinkService;
     }
 
-    // public GetDoughByNameResponse getDough(String name) {
-    //     return doughServices.getDoughByName(name);
-    // }
-
+    
     public Menu[] getMenu() {
         return Menu.values();
     }
